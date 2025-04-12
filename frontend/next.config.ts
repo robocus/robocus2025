7ee03b6bee4e13c13611f5cb1ba114e9bb184d83
anh.nguyen <anh.nguyen@team.eyeq.tech>
2025-04-12 17:34:17 +0700

    test automation build & deploy

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+const nextConfig = {
   output: 'export',
+  basePath: '',
+  assetPrefix: '',
+  eslint: { ignoreDuringBuilds: true },
 };
 
 export default nextConfig;
